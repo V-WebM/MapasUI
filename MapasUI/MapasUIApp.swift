f977a840de6c52f1d1f4d44689f6826c651bd9b2
alvaro270 <alvarogamer270@gmail.com>
2022-06-23 18:34:40 -0500

    Initial Commit

---
+++ MapasUI/MapasUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapasUIApp.swift
+//  MapasUI
+//
+//  Created by MAC30 on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MapasUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
