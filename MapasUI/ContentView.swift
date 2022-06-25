e56f6abd77555fe8b71578233b0e57312407d13a
Ventura <marlenp363@gmail.com>
2022-06-24 20:31:16 -0500

    primer commit

--- MapasUI/ContentView.swift
+++ MapasUI/ContentView.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: -12.04318, longitude: -77.02824),
+    span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+    )
+    
+    private let places: [Location] = [
+        Location(coordinate: CLLocationCoordinate2D(latitude: -12.04318, longitude: -77.02824))
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region, annotationItems: places, annotationContent: { place in MapPin(coordinate: place.coordinate, tint: .green)
+        })
     }
+}
+
+struct Location: Identifiable {
+    let id = UUID()
+    var coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView_Previews: PreviewProvider {
